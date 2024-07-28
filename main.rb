87f105886fa41da37b229a24d257019fba030e28
Zachary Arledge <174385639+vlamm0@users.noreply.github.com>
2024-07-28 11:59:59 -0400

    Get string from user

---
+++ main.rb
@@ -0,0 +1,10 @@
+DICTIONARY = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+# gets input string from user
+def getString()
+  puts "Enter string to cross-check with dictionary"
+  input = gets.chomp.downcase
+  puts input
+end
+
+
