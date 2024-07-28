97f30570ca87159b0dba3e0d32c659f7f7773da1
Zachary Arledge <174385639+vlamm0@users.noreply.github.com>
2024-07-28 13:29:53 -0400

    Check sentence against dictionary

--- main.rb
+++ main.rb
@@ -3,17 +3,30 @@ DICTIONARY = ["below","down","go","going","horn","how","howdy","it","i","low","o
 # gets input string from user
 def getString()
   puts "Enter string to cross-check with dictionary"
-  input = gets.chomp.downcase
-  puts input
+  input = gets.chomp
+  return input
 end
 
-def substring(string)
-  lookupHash = Hash.new
-  if DICTIONARY.include?(string)
-    lookupHash[string] = 1
+def formatString(string)
+  string.downcase!
+  string.gsub!(/[^\w\s]/, '')
+  formattedStirng = string.split(" ")
+  return formattedStirng
+end
+
+def substring(string, dictionary)
+  #lookupHash = Hash.new
+  arr = formatString(string)
+  p arr
+  arr.reduce(Hash.new(0)) {|lookupHash, word| 
+  p lookupHash
+  if dictionary.include?(word)
+    lookupHash[word] += 1
   end
-  return lookupHash
+  lookupHash
+  }
+  #return lookupHash
 end
 
-p substring("go")
-
+p substring("Howdy partner, sit down! How's it going?", DICTIONARY)
+#Howdy partner, sit down! How's it going?"
